8cb328441266b9146f7e4393093d1fd076706f52
mtfortunelli <fortunellimatteo@gmail.com>
2015-03-03 18:27:38 +0100

    descrizione

---
+++ primoesercizio.sh
@@ -0,0 +1,10 @@
+read numero1
+read numero2
+read n
+
+for ((i=1;i<n;i++))
+do
+somma=$(($numero1+$numero2))
+done
+
+echo somma
---
+++ primoprogramma.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+main()
+{
+	int n,numero1,numero2,i,somma;
+	printf("inserire il vettore:");
+	scanf("%d",n);	
+	printf("inserire il primo numero:");
+	scanf("%d",numero1);
+	printf("inserire il secondo numero:");
+	scanf("%d",numero2);
+	
+	for(i=0;i<n;i++){
+		somma=numero1+numero2;
+	}
+	printf("somma:%f",somma);
+    
+    printf("\n\n");
+system("PAUSE");
+}
